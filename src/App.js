d64b31b481b93022af13552628c5e978b8e66626
S. D. E. A. Gunathilaka <87906517+EshanAdithaya@users.noreply.github.com>
2024-12-20 11:42:30 +0530

    uncomment ShopDetail route and comment out ShopsPage route in App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import ShopListing from './pages/ShopListing';
-// import ShopDetail from './pages/ShopDetail';
+import ShopDetail from './pages/ShopDetail';
 import OrderDetailsScreen from './pages/OrderDetailsScreen';
 import Layout from './components/Layout';
 import PromotionsPage from './pages/PromotionsPage';
@@ -28,7 +28,7 @@ function App() {
           <Route path="/" element={<ShopListing />} />
           <Route path="/test" element={<ShopListing />} />
           <Route path="/aa" element={<OrderDetailsScreen />} />
-          {/* <Route path="/shops" element={<ShopDetail />} /> */}
+          <Route path="/shops" element={<ShopDetail />} />
           <Route path='/offers' element={<PromotionsPage />} />
           <Route path='/flash-sale' element={<FlashSalesPage />} />
           <Route path='/faq' element={<FAQPage />} />
@@ -37,7 +37,7 @@ function App() {
           
           {/* left side nav bar ? */}
           <Route path="/contact" element={<ContactPage />} />
-          <Route path="/shops" element={<ShopsPage />} />
+          {/* <Route path="/shops" element={<ShopsPage />} /> */}
           <Route path="/manufacturers" element={<ManufacturersPage />} />
 
 
