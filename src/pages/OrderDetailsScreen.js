8174a6b5cd5750c782797d42d2f954114fbaef64
S. D. E. A. Gunathilaka <87906517+EshanAdithaya@users.noreply.github.com>
2025-02-27 02:45:49 +0530

    fix: uncomment refund section in OrderDetailsScreen for future implementation

--- src/pages/OrderDetailsScreen.js
+++ src/pages/OrderDetailsScreen.js
@@ -271,7 +271,7 @@ const OrderDetailsScreen = () => {
         </div>
 
         {/* Refund Section */}
-        {/* <div className="px-6 py-4 -mx-6">
+        <div className="px-6 py-4 -mx-6">
           {refund ? (
             <div className="bg-yellow-50 border border-yellow-200 rounded-lg p-4 mb-4">
               <div className="flex items-center justify-between">
@@ -315,7 +315,7 @@ const OrderDetailsScreen = () => {
               </button>
             )
           )}
-        </div> */}
+        </div>
 
         <div className="mt-6">
           <div className="flex justify-between py-3 px-4 bg-gray-50 rounded-t-lg">
