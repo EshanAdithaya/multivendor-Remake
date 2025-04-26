3d63e7da91aae4b569f6630903abc84937ded043
S. D. E. A. Gunathilaka <87906517+EshanAdithaya@users.noreply.github.com>
2025-04-26 14:56:53 +0530

    refactor: comment out cart item count display in Navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -220,11 +220,11 @@ const NavigationWithMenus = () => {
           }`}
         >
           <ShoppingBag className="w-6 h-6" />
-          {cartCount > 0 && (
+          {/* {cartCount > 0 && (
             <span className="absolute -top-2 -right-2 bg-red-500 text-white text-xs rounded-full w-5 h-5 flex items-center justify-center">
               {cartCount > 99 ? '99+' : cartCount}
             </span>
-          )}
+          )} */}
         </button>
         <button 
           onClick={toggleRightSidebar}
